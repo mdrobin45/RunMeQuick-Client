0b4daf8ad3424031d90be7559f26ae10ec503522
Md Robin Rana <developer.smrobin45@gmail.com>
2024-01-01 09:10:19 +0600

    initial editor setup

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,8 +1,33 @@
+import EditorWindow from "../../Components/EditorWindow/EditorWindow";
+
 const Landing = () => {
    return (
-      <div>
-         <h2>Landing</h2>
-      </div>
+      <section className="flex items-center gap-10 p-10">
+         <div className=" w-2/3">
+            <div className="flex items-center gap-4">
+               <select
+                  id="countries"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option selected>Choose a country</option>
+                  <option value="US">United States</option>
+                  <option value="CA">Canada</option>
+                  <option value="FR">France</option>
+                  <option value="DE">Germany</option>
+               </select>
+               <select
+                  id="countries"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option selected>Choose a country</option>
+                  <option value="US">United States</option>
+                  <option value="CA">Canada</option>
+                  <option value="FR">France</option>
+                  <option value="DE">Germany</option>
+               </select>
+            </div>
+            <EditorWindow />
+         </div>
+         <div></div>
+      </section>
    );
 };
 
