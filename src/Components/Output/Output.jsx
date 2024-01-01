3b402c21f30413e2e8d1fce2d5356f18dbccc270
Md Robin Rana <developer.smrobin45@gmail.com>
2024-01-01 09:35:11 +0600

    output section added

---
+++ src/Components/Output/Output.jsx
@@ -0,0 +1,12 @@
+const Output = () => {
+   return (
+      <div className="mt-6 h-full">
+         <h2 className="text-2xl font-bold pb-2">Output</h2>
+         <div className="bg-gray-800 text-[#07bc0c] w-full h-48 p-4">
+            <span>Hello world</span>
+         </div>
+      </div>
+   );
+};
+
+export default Output;
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,32 +1,37 @@
 import EditorWindow from "../../Components/EditorWindow/EditorWindow";
+import Output from "../../Components/Output/Output";
 
 const Landing = () => {
    return (
-      <section className="flex items-center gap-10 p-10">
-         <div className=" w-2/3">
-            <div className="flex items-center gap-4">
-               <select
-                  id="countries"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option selected>Choose a country</option>
-                  <option value="US">United States</option>
-                  <option value="CA">Canada</option>
-                  <option value="FR">France</option>
-                  <option value="DE">Germany</option>
-               </select>
-               <select
-                  id="countries"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option selected>Choose a country</option>
-                  <option value="US">United States</option>
-                  <option value="CA">Canada</option>
-                  <option value="FR">France</option>
-                  <option value="DE">Germany</option>
-               </select>
-            </div>
-            <EditorWindow />
+      <section className=" p-10">
+         <div className="flex items-center gap-4">
+            <select
+               id="countries"
+               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+               <option selected>Choose a country</option>
+               <option value="US">United States</option>
+               <option value="CA">Canada</option>
+               <option value="FR">France</option>
+               <option value="DE">Germany</option>
+            </select>
+            <select
+               id="countries"
+               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+               <option selected>Choose a country</option>
+               <option value="US">United States</option>
+               <option value="CA">Canada</option>
+               <option value="FR">France</option>
+               <option value="DE">Germany</option>
+            </select>
          </div>
-         <div></div>
+         <div className="flex gap-3">
+            <div className="w-3/4">
+               <EditorWindow />
+            </div>
+            <div className="w-[25%] h-48">
+               <Output />
+            </div>
+         </div>
       </section>
    );
 };
